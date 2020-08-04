d93c557bb3ce8c545552ed65bbe086636508e3cb
Tommaso <tommaso.vsr@gmail.com>
2020-08-04 14:09:16 +0200

    righe collapsable in costruzione

--- src/components/Menu/MenuTable.js
+++ src/components/Menu/MenuTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,6 +7,9 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -60,22 +63,35 @@ const days = [
 ]
 
 const useStyles = makeStyles({
-  table: {
-    width: '80%',
-    height: '90vh',
-    float: 'right'
-  },
-  meal: {
-    padding: '16px 0'
-  }
+    container: {
+        maxHeight: '90vh',
+        overflow: 'auto'
+    },
+    table: {
+        width: '80%',
+        maxHeight: '90vh',
+        float: 'right'
+    },
+    meal: {
+        padding: '16px 0'
+    },
+    subrow: {
+      visibility: 'visible' //toggle to 'collapse' on click
+    }
 });
+
+
+const handleCollapse = () => {
+  console.log('this.state.open changed!')
+}
 
 export default function CustomizedTables() {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} className={classes.container}>
       <Table className={classes.table} aria-label="customized table">
+
         <TableHead>
           <TableRow>
               {
@@ -83,36 +99,57 @@ export default function CustomizedTables() {
               }
           </TableRow>
         </TableHead>
+
         <TableBody>
           {
             rows.map((row) => (
+              <Fragment>
                 <StyledTableRow key={row.name}>
                     <StyledTableCell component="th" scope="row">
                         {row.name}
-                    </StyledTableCell>
                         {
-                            Object.keys(row).map(day => {
-                                return day === 'name' 
-                                ? null
-                                : (
-                                    <StyledTableCell>
-                                        {
-                                            row.lun.map(each => {
-                                                return (
-                                                    <TableRow>
-                                                        <TableCell className={classes.meal}>{each}</TableCell>
-                                                    </TableRow>
-                                                );
-                                            })
-                                        }
-                                    </StyledTableCell>
-                                );
-                            })
+                            row.name === 'Primi Piatti' || row.name === 'Secondi Piatti'
+                            ? (
+                                    <IconButton aria-label="expand row" size="small" onClick={() => handleCollapse()}>
+                                        {true ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                                    </IconButton> 
+                            )
+                            : null
                         }
+                    </StyledTableCell>
+                    {
+                        Object.keys(row).map(day => {
+                            return day === 'name' 
+                            ? null
+                            : (
+                                <StyledTableCell>
+                                    {
+                                        row.lun.map(each => {
+                                            return (
+                                                <TableRow>
+                                                    <TableCell className={classes.meal}>{each}</TableCell>
+                                                </TableRow>
+                                            );
+                                        })
+                                    }
+                                </StyledTableCell>
+                            );
+                        })
+                    }
                 </StyledTableRow>
+                <Fragment>
+                  {
+                    row.name === 'Primi Piatti' || row.name === 'Secondi Piatti'
+                    ? <StyledTableRow className={classes.subrow}>ciao</StyledTableRow>
+                    : null
+                  }
+                </Fragment>
+              </Fragment>
+                
             ))
           }
         </TableBody>
+
       </Table>
     </TableContainer>
   );
